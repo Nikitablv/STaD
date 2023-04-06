46d400e963b52c2335470084d2f1e05025a4c31f
Nikitablv <80066784+Nikitablv@users.noreply.github.com>
2023-04-06 21:58:40 +0300

    Add files via upload

---
+++ LW9/LW9/App.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LW9
+{
+    public class App
+    {
+        public App()
+        {
+            client = new(new HttpClientHandler())
+            {
+                BaseAddress = new Uri(baseUrl)
+            };
+        }
+
+        public async Task<JArray> GetProducts()
+        {
+            HttpResponseMessage response = await client.GetAsync(baseUrl + getProducts);
+
+            return JArray.Parse(JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync())!.ToString());
+        }
+
+        public async Task<JObject> CreateProduct(Product product)
+        {
+            string jsonObject = JsonConvert.SerializeObject(product);
+            StringContent requestContent = new StringContent(jsonObject, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync(baseUrl + addProduct, requestContent);
+
+            return JObject.Parse(JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync()).ToString());
+        }
+
+        public async Task<JObject> DeleteProduct(int idProduct)
+        {
+            HttpResponseMessage response = await client.GetAsync(baseUrl + deleteProduct + $"?id={idProduct}");
+
+            return JObject.Parse(JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync()).ToString());
+        }
+
+        public async Task<JObject> EditProduct(Product product)
+        {
+            string jsonObject = JsonConvert.SerializeObject(product);
+            StringContent requestContent = new StringContent(jsonObject, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync(baseUrl + updateProduct, requestContent);
+
+            return JObject.Parse(JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync()).ToString());
+        }
+
+        private readonly string baseUrl = "http://shop.qatl.ru/";
+        private readonly string getProducts = "api/products";
+        private readonly string addProduct = "api/addproduct";
+        private readonly string deleteProduct = "api/deleteproduct";
+        private readonly string updateProduct = "api/editproduct";
+        private readonly HttpClient client;
+    }
+}
---
+++ LW9/LW9/Product.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace LW9
+{
+    public class Product
+    {
+        public int? id { get; set; }
+        public int? category_id { get; set; }
+        public string? title { get; set; }
+        public string? alias { get; set; }
+        public string? content { get; set; }
+        public int? price { get; set; }
+        public int? old_price { get; set; }
+        public int? status { get; set; }
+        public string? keywords { get; set; }
+        public string? description { get; set; }
+        public int? hit { get; set; }
+    }
+}
